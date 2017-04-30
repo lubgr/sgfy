3066430aa1a86a7102ae024dddfeb4a60b341931
Lukas Böger <dev@lboeger.de>
2017-05-01 00:13:26 +0200

    implement split of fmt-string and setup recursion

--- sgfy.h
+++ sgfy.h
@@ -1,11 +1,85 @@
 
 #include <string>
+#include <cstring>
+#include <regex>
+#include <sstream>
+#include <iostream>
+#include <cassert>
 
 namespace sgfy {
-    template<class ...T> std::string str(const char *fmt, const T&... args)
-    {
-        std::string fmtStr(fmt);
+    std::ptrdiff_t getNumFmtSpecifier(const std::string& fmt);
+    template<class S, class ...T> void splitAndAppend(std::ostream& stream,
+            const std::string& nextFmt, const S& firstArg, const T&... args);
 
-        return fmtStr;
+    void shiftOrContinue(std::ostream& stream, std::ptrdiff_t& nLeft, const std::string& nextFmt)
+    {
+        assert(nLeft == 0);
+
+        (void) nLeft;
+
+        stream << nextFmt;
+    }
+
+    template<class S, class... T> void shiftOrContinue(std::ostream& stream, std::ptrdiff_t& nLeft,
+            const std::string& nextFmt, const S& firstArg, const T&... args)
+    {
+        if (nLeft > 0)
+            shiftOrContinue(stream, --nLeft, nextFmt, args...);
+        else
+            splitAndAppend(stream, nextFmt, firstArg, args...);
+    }
+
+    template<class S, class ...T> void append(std::ostream& stream, const std::string& fmt,
+            const std::string& nextFmt, const S& firstArg, const T&... args)
+    {
+        std::ptrdiff_t n = getNumFmtSpecifier(fmt);
+
+        stream << fmt;
+
+        shiftOrContinue(stream, n, nextFmt, firstArg, args...);
+    }
+
+    std::ptrdiff_t getNumFmtSpecifier(const std::string& fmt)
+        /* The regex pattern are constructed following the docs of std::printf. */
+    {
+        return 0;
+    }
+
+    template<class ...T> void splitAndAppend(std::ostream& stream, const std::string& fmt)
+    {
+        stream << fmt;
+    }
+
+    template<class S, class ...T> void splitAndAppend(std::ostream& stream, const std::string& fmt,
+            const S& firstArg, const T&... args)
+    {
+        const size_t pos = fmt.find("%S");;
+
+        if (fmt.length() == 0)
+            return;
+        else if (pos == std::string::npos)
+            append(stream, fmt, "", firstArg, args...);
+        else if (pos == 0) {
+            stream << firstArg;
+            splitAndAppend(stream, fmt.substr(2), args...);
+        } else
+            append(stream, fmt.substr(0, pos), fmt.substr(pos), firstArg, args...);
+    }
+
+    template<class S, class ...T> std::string str(const std::string& fmt, const S& firstArg,
+            const T&... args)
+    {
+        std::stringstream stream;
+
+        assert(fmt.find("%") != std::string::npos);
+
+        splitAndAppend(stream, fmt, firstArg, args...);
+
+        return stream.str();
+    }
+
+    template<class ...T> std::string str(const std::string& termination)
+    {
+        return termination;
     }
 }
--- tests.cpp
+++ tests.cpp
@@ -1,14 +1,62 @@
 
+#include <sstream>
 #include "sgfy.h"
-#include "CppUTest/TestHarness.h"
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
 
 using namespace sgfy;
 
-TEST_GROUP(Sgfy) {};
+const std::string objStr = "[obj]";
+
+struct Class {} obj;
+
+std::ostream& operator << (std::ostream& stream, const Class& rhs)
+{
+    stream << objStr;
+
+    return stream;
+}
+
+TEST_GROUP(Sgfy)
+{
+};
+
+TEST(Sgfy, emptyString)
+{
+    const std::string result(str(""));
+
+    CHECK(result.empty());
+}
+
+TEST(Sgfy, printfFmtWithoutArgs)
+{
+    const char *fmt = "%d %s %x";
+
+    CHECK_EQUAL(fmt, str(fmt));
+}
 
 TEST(Sgfy, withOutPlaceholder)
 {
     const char *plainString = "String without format specifier";
 
     CHECK_EQUAL(plainString, str(plainString));
+}
+
+TEST(Sgfy, exclusivelyStreamOperator)
+{
+    const std::string result(str("%S", obj));
+
+    CHECK_EQUAL(objStr, result);
+}
+
+TEST(Sgfy, onlyStreamOperator)
+{
+    const std::string result(str("before %S between %S after", obj, obj));
+
+    CHECK_EQUAL("before " + objStr + " between " + objStr + " after", result);
+}
+
+int main(int argc, char **argv)
+{
+    return CommandLineTestRunner::RunAllTests(argc, argv);
 }
